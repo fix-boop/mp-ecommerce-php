aafd774765587ad259c026b4e7660fc24a961824
Jesús Ruiz <fixboop@gmail.com>
2021-02-26 04:02:00 -0600

    a

--- test.php
+++ test.php
@@ -1,10 +1,14 @@
 <?php
     require 'vendor/autoload.php';
     require_once 'credenciales.php';
+    if (isset($_POST["type"])) {
+        http_response_code(200);
+    }
+    
     MercadoPago\SDK::setAccessToken($access_token);
   $payment = MercadoPago\Payment::find_by_id($_GET['id']);
   echo '<pre>';
-  var_dump($payment);
+  //var_dump($payment);
   var_dump(json_encode($payment));
   echo '</pre>';
 ?>
