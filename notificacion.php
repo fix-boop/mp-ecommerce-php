224879485061cf26d48229b28dfdb5ad6b917abb
Jesús Ruiz <fixboop@gmail.com>
2021-02-26 06:12:47 -0600

    a

--- notificacion.php
+++ notificacion.php
@@ -3,9 +3,23 @@ require 'vendor/autoload.php';
 require_once 'credenciales.php';
 MercadoPago\SDK::setAccessToken($access_token);
 
-// Receive the JSON Post data in form of array
-$data = json_decode( file_get_contents( 'php://input' ), true ); 
 
+// obtenemos el contenido del request
+$notification = file_get_contents('php://input');
+
+    //$dbHandler = new DbHandler();
+    
+    //$respuesta = $dbHandler->insertNotification($notification);
+    
+    $dbHandler = DbHandler::getInstancia();
+    $respuesta = $dbHandler->insertNotification($notification);
+    
+    if($respuesta["status"] == 400) http_response_code(400);
+    
+    // devolvemos una respuesta con estado 200 al cliente
+    http_response_code(200);
+    
+    var_dump(json_encode($respuesta));
 
 if (isset($_POST["type"])) {
     //http_response_code(200);
