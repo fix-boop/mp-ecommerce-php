d30143337b812c55e04479dd2fe28b5b172f1742
Jesús Ruiz <fixboop@gmail.com>
2021-02-26 03:47:28 -0600

    a

--- notificacion.php
+++ notificacion.php
@@ -10,7 +10,7 @@ if (isset($_POST["type"])) {
 
    switch($_POST["type"]) {
     case "payment":
-        $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+        $payment = MercadoPago\Payment.find_by_id($_POST["data.id"]);
         // Guardar en BD, pago procesado
         if (file_exists("datos.txt")){
         $archivo = fopen("tmp/datos.txt", "a");
@@ -23,6 +23,16 @@ if (isset($_POST["type"])) {
         fclose($archivo);
         }
         // Enviar mail al comprador, informado resultado
+
+
+      $para = 'fixboop@gmail.com';
+      $titulo = 'WebHook';
+      $mensaje = json_encode($payment);
+      $cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+      $cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+      $cabeceras .= 'From: fixboop.com <no-reply@yolobusko.com>' . "\r\n";
+
+      mail($para, $titulo, $mensaje, $cabeceras);
         break;
     case "plan":
         $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
--- success.php
+++ success.php
@@ -6,7 +6,7 @@
 <script src="https://www.mercadopago.com/v2/security.js" view=""></script>
 </head>
 <body>
-<p>PAYMENT METHOD ID: <?php echo isset($_GET['payment_method_id']) ? $_GET['payment_method_id'] : '';?></p>
+<p>PAYMENT TYPE: <?php echo isset($_GET['payment_type']) ? $_GET['payment_type'] : '';?></p>
 <p>EXTERNAL REFERENCE: <?php echo isset($_GET['external_reference']) ? $_GET['external_reference'] : '';?></p>
 <p>PAYMENT ID: <?php echo isset($_GET['collection_id']) ? $_GET['collection_id'] : '';?></p>
 <p>SUCCESS</p>
