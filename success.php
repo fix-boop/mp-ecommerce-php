8d755087055693a711ba3b78562b986178567678
Jesús Ruiz <fixboop@gmail.com>
2021-02-26 03:27:26 -0600

    a

--- success.php
+++ success.php
@@ -6,9 +6,9 @@
 <script src="https://www.mercadopago.com/v2/security.js" view=""></script>
 </head>
 <body>
+<p>PAYMENT METHOD ID: <?php echo isset($_GET['payment_method_id']) ? $_GET['payment_method_id'] : '';?></p>
 <p>EXTERNAL REFERENCE: <?php echo isset($_GET['external_reference']) ? $_GET['external_reference'] : '';?></p>
 <p>PAYMENT ID: <?php echo isset($_GET['collection_id']) ? $_GET['collection_id'] : '';?></p>
-<p>PAYMENT TYPE: <?php echo isset($_GET['payment_type']) ? $_GET['payment_type'] : '';?></p>
 <p>SUCCESS</p>
 </body>
 </html>
