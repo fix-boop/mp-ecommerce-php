463eaf0ed317b0173ed8be25620904e8ff282b6a
Jesús Ruiz <fixboop@gmail.com>
2021-02-26 02:42:57 -0600

    a

--- notificacion.php
+++ notificacion.php
@@ -32,19 +32,20 @@ if (isset($_POST["type"])) {
     http_response_code(200);
 }
 
+$json = file_get_contents('php://input');
+
    switch($_POST["type"]) {
     case "payment":
         $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-        
         // Guardar en BD, pago procesado
         if (file_exists("datos.txt")){
         $archivo = fopen("tmp/datos.txt", "a");
-        fwrite($archivo, PHP_EOL . $payment );
+        fwrite($archivo, PHP_EOL . $payment.$json );
         fclose($archivo);
         }
         else {
         $archivo = fopen("datos.txt", "w");
-        fwrite($archivo, PHP_EOL . $payment );
+        fwrite($archivo, PHP_EOL . $payment.$json );
         fclose($archivo);
         }
         // Enviar mail al comprador, informado resultado
