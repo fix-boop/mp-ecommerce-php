3f092f4fbe9cefad7fa1b7a05ba9e665fe5e64da
Jesús Ruiz <fixboop@gmail.com>
2021-02-26 04:15:17 -0600

    a

--- detail.php
+++ detail.php
@@ -59,8 +59,8 @@ $preference->back_urls = array(
 );
 $preference->auto_return = "approved";
 
-$preference->notification_url = "https://ruizpac-mp-commerce-php.herokuapp.com/notification.php?source_news=webhooks";
-//$preference->notification_url = "https://ruizpac-mp-commerce-php.herokuapp.com/notification.php";
+//$preference->notification_url = "https://ruizpac-mp-commerce-php.herokuapp.com/notification.php?source_news=webhooks";
+$preference->notification_url = "https://ruizpac-mp-commerce-php.herokuapp.com/notification.php";
 $preference->save();
 
 //guardo preferencia
--- notificacion.php
+++ notificacion.php
@@ -1,18 +1,27 @@
 <?php
+
+    $json = file_get_contents('php://input');
+
+    $json_url = json_decode($json);
+
+    $logFile = fopen("log.txt", 'a') or die("Error creando archivo");
+    fwrite($logFile, "\n\n".date("d/m/Y H:i:s")."JSON_URL: ".print_r( $json_url )) or die("Error escribiendo en el archivo");
+    fclose($logFile);
+
 require 'vendor/autoload.php';
 require_once 'credenciales.php';
 
 MercadoPago\SDK::setAccessToken($access_token);
 
 if (isset($_POST["type"])) {
-    http_response_code(200);
+    //http_response_code(200);
 }
 
    switch($_POST["type"]) {
     case "payment":
         $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
         // Guardar en BD, pago procesado
-        if (file_exists("datos.txt")){
+       /* if (file_exists("datos.txt")){
         $archivo = fopen("tmp/datos.txt", "a");
         fwrite($archivo, PHP_EOL . $payment );
         fclose($archivo);
@@ -21,7 +30,7 @@ if (isset($_POST["type"])) {
         $archivo = fopen("datos.txt", "w");
         fwrite($archivo, PHP_EOL . $payment );
         fclose($archivo);
-        }
+        }*/
         // Enviar mail al comprador, informado resultado
         break;
     case "plan":
@@ -33,6 +42,11 @@ if (isset($_POST["type"])) {
     case "invoice":
         $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
         break;
+    }
+    if (isset($payment)) {
+        $logFile = fopen("log.txt", 'a') or die("Error creando archivo");
+        fwrite($logFile, "\n".date("d/m/Y H:i:s")."JSON_payment: ".print_r($payment)) or die("Error escribiendo en el archivo");
+        fclose($logFile);
     }
 
 ?>
